d644ff98dec4458f327877cf783686e21e5285b5
Jason Li <lizhansong@pm.me>
2026-07-04 03:06:08 +1000

    improve drawing

--- RE2InventoryOverlayNonBinarySearch.lua
+++ RE2InventoryOverlayNonBinarySearch.lua
@@ -246,6 +246,7 @@ local settings = {
     padding = 10,
     margin_top = 240,
     margin_left = 40,
+    margin_between = 5,
     textsize = 16
 }
 local settingsfile = "NonBinarySearchRE2InventoryOverlay.json"
@@ -268,6 +269,7 @@ local function load_settings()
         settings.padding = rt(sf, "padding", settings.padding)
         settings.margin_top = rt(sf, "margin_top", settings.margin_top)
         settings.margin_left = rt(sf, "margin_left", settings.margin_left)
+        settings.margin_between = rt(sf, "margin_between", settings.margin_between)
         settings.textsize = rt(sf, "textsize", settings.textsize)
     else
         save_settings()
@@ -280,6 +282,7 @@ load_settings()
 local margin_top = nil
 local margin_left = nil
 local padding = nil
+local margin_between = nil
 local uiscale = nil
 local initialized = false
 local image_loading_error = false
@@ -335,6 +338,7 @@ local function inventory_overlay_d2d_initialize()
     padding = settings.padding
     margin_top = settings.margin_top
     margin_left = settings.margin_left
+    margin_between = settings.margin_between
 
     fontsize = math.floor(settings.textsize * uiscale)
     font = d2d.Font.new("NotoSansMono-SemiBold.ttf", fontsize)
@@ -354,56 +358,64 @@ end
 
 local function get_base_coord(col, row)
     -- top left
-    return margin_left + col * unit, margin_top + row * unit + (row - 1) * padding
+    return margin_left + col * unit + col * margin_between, margin_top + row * unit + row * margin_between
 end
 
 local function draw_image(image, col, row)
     local w,h = image:size()
-    local adjusted_w = math.floor(w * uiscale)
-    local adjusted_h = math.floor(h * uiscale)
+    local adjusted_w = math.floor(w * uiscale) - padding * 2
+    local adjusted_h = math.floor(h * uiscale) - padding * 2
     local x,y = get_base_coord(col, row)
     d2d.image(
         image,
-        x, y,
+        x + padding, y + padding / 2,
         adjusted_w, adjusted_h
     )
-    return x,y,adjusted_w, adjusted_h
+    return x + padding, y + padding, adjusted_w, adjusted_h
 end
 
-local function draw_text_below_image(s, x, y, w, h)
+local function draw_text_below_image(s, col, row, is_fat)
     text_height = fontsize * 0.65
-    text_width = string.len(s) * fontsize * 0.55
+    text_width = string.len(s) * fontsize * 0.45
+    local x, y = get_base_coord(col, row)
+    local h = unit
+    local w = unit
+    if is_fat then
+        w = unit * 2 + margin_between
+    end
+
     draw_text_shadowed(
         s,
-        x + math.floor(w/2 - text_width/2), y + h - math.floor(text_height + 5 * uiscale)
+        x + w / 2 - text_width / 2,
+        y + h - padding - text_height
     )
 end
 
 local function draw_item(image, col, row, item_number, item_state)
     local x,y,w,h = draw_image(image, col, row)
     if item_state.is_infinite_use and not item_state.is_removable then
-        draw_text_below_image("[K]",x,y,w,h)
+        draw_text_below_image("[K]", col, row, false)
     elseif not item_state.is_infinite_use then
-        draw_text_below_image(tostring(item_number),x,y,w,h)
+        draw_text_below_image(tostring(item_number), col, row, false)
     end
 end
 
-local function draw_weapon(image, col, row, current_ammo, max_ammo, is_equip)
+local function draw_weapon(image, col, row, weapon_state)
     local x,y,w,h = draw_image(image, col, row)
     local s = ""
-    if max_ammo < 0 and current_ammo < 0 then
+    if weapon_state.max_ammo < 0 and weapon_state.current_ammo < 0 then
         s = "inf"
-    elseif max_ammo < 0 then
-        s = string.format("%d/inf", current_ammo)
+    elseif weapon_state.max_ammo < 0 then
+        s = string.format("%d/inf", weapon_state.current_ammo)
     else
-        s = string.format("%d/%d", current_ammo, max_ammo)
+        s = string.format("%d/%d", weapon_state.current_ammo, weapon_state.max_ammo)
     end
 
-    if is_equip then
+    if weapon_state.is_equip then
         s = s .. "[E]"
     end
 
-    draw_text_below_image(s,x,y,w,h)
+    draw_text_below_image(s, col, row, weapon_state.is_fat)
 end
 
 local function draw_border(col, row, is_fat)
@@ -413,9 +425,9 @@ local function draw_border(col, row, is_fat)
     local x,y = get_base_coord(col, row)
     local w = unit
     if is_fat then
-        w = unit * 2
+        w = unit * 2 + margin_between
     end
-    local h = unit + padding
+    local h = unit
     local thickness = 1
 
     d2d.outline_rect(x + shadowoffset, y + shadowoffset, w, h, thickness, sc)
@@ -459,6 +471,7 @@ local function inventory_overlay_d2d_draw()
 
         for col=0,ncols-1 do
             local slot = inventory:call("getSlot", index)
+            local is_fat = slot:call("get_IsFatSlot")
             if slot:call("get_IsItem") then
                 local item_id = slot:call("get_ItemID")
                 local item_number = slot:call("get_Number")
@@ -481,11 +494,16 @@ local function inventory_overlay_d2d_draw()
                 if not image then
                     draw_error(col, row, slot:call("get_IsFatSlot"))
                 else
-                    draw_weapon(image, col, row, current_ammo, max_ammo, is_equip)
+                    draw_weapon(image, col, row, {
+                        current_ammo = current_ammo,
+                        max_ammo = max_ammo,
+                        is_equip = is_equip,
+                        is_fat = is_fat
+                    })
                 end
             end
 
-            if slot:call("get_IsFatSlot") then
+            if is_fat then
                 draw_border(col, row, true)
             elseif not slot:call("get_IsDeadSlot") then
                 draw_border(col, row, false)
